d23cd7c5c13569f4f244dd86f2496f9854eb713e
Wilhelm Capraro <wilhelm.capraro@gmail.com>
2024-02-07 21:41:33 +0100

    new content for double app

--- src/VerdoppelnApp.java
+++ src/VerdoppelnApp.java
@@ -3,10 +3,13 @@ public class VerdoppelnApp {
     public static void main(String[] args) {
         int n = 13;
         int x = 2;
-
+        int max = 32;
         for (int i=0; i < n; i++) {
             System.out.println(x);
             x = x * 2;
+            if (x > max) {
+                break;
+            }
         }
     }
 }
